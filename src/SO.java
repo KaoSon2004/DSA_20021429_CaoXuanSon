22e150f2e4a8a9ae3fc4f8e8dfcfcb141291318b
KaoSon2004 <gsu0987q@gmail.com>
2021-09-29 09:03:56 +0700

    first commit

---
+++ src/SO.java
@@ -0,0 +1,16 @@
+public class SO {
+    public SO() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("*****  *****  *        *");
+        System.out.println("*      *   *  **       *");
+        System.out.println("*      *   *  * *      *");
+        System.out.println("*      *   *  *  *     *");
+        System.out.println("*****  *   *  *   *    *");
+        System.out.println("    *  *   *  *    *   *");
+        System.out.println("    *  *   *  *      * *");
+        System.out.println("    *  *   *  *       **");
+        System.out.println("*****  *****  *        *");
+    }
+}
