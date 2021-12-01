6c501cd5a069adb68a0fdb15e608f6695b5872f0
KaoSon2004 <gsu0987q@gmail.com>
2021-12-01 15:32:58 +0700

    w12

---
+++ Week12/src/ConnectedCells.java
@@ -0,0 +1,68 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ConnectedCells {
+
+    private static int rows;
+    private static int cols;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        rows = scan.nextInt();
+        cols = scan.nextInt();
+        int grid[][] = new int[rows][cols];
+        boolean visited[][] = new boolean[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                visited[i][j] = false;
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+
+        System.out.println(largestRegion(grid, visited));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(visited[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int largestRegion(int [][] grid, boolean [][] visited) {
+        int maxRegion = 0;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (grid[row][col] == 1 && visited[row][col] == false) {
+                    int size = count_connected(grid, row, col, visited);
+                    maxRegion = Math.max(maxRegion, size);
+                }
+            }
+        }
+        return maxRegion;
+    }
+
+    private static int count_connected(int [][] grid, int row, int col, boolean [][] visited) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) {
+            return 0;
+        } else if (visited[row][col] == true || grid[row][col] == 0) {
+            return 0;
+        }
+
+        visited[row][col] = true;
+        int size = 1;
+
+        for (int r = row - 1; r <= row + 1; r++) {
+            for (int c = col - 1; c <= col + 1; c++) {
+                size += count_connected(grid, r, c, visited);
+            }
+        }
+
+        return size;
+    }
+}
